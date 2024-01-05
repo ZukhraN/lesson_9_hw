46ba3edb6bf4bb836bb2d1c417a26761a4040674
Zukhra Nurmanova <zukhra.nurmanova@gmail.com>
2024-01-05 10:56:13 +0600

    Initial commit

---
+++ tests/test_selene.py
@@ -0,0 +1,20 @@
+from selene.support import by
+from selene.support.conditions import be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+
+def test_github():
+    browser.open("https://github.com")
+    browser.driver.maximize_window()
+
+    s(".search-input-container").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    s("#query-builder-test").submit()
+
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s("#issues-tab").click()
+
+    s(by.partial_text("#76")).should(be.visible)
